263c3cd89153ffd36891487c89632892a83d1367
Gabriel Grecu <grecualexandrugabriel@yahoo.com>
2024-06-06 14:38:36 +0300

    adding first class

---
+++ Java-RoadMap/src/main/java/com/roadmap/JavaRoadMapApplication.java
@@ -0,0 +1,13 @@
+package com.roadmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRoadMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRoadMapApplication.class, args);
+	}
+
+}
